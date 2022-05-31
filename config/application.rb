384381c48e5f7a72f53d3c91290612ce26772012
derek.t <derek@risingcloud.com>
2022-05-31 10:54:40 -0700

    add hosts

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module RubyWebService
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.hosts << '.dev.risingcloud.app'
+    config.hosts << '.risingcloud.app'
 
     # Configuration for the application, engines, and railties goes here.
     #
